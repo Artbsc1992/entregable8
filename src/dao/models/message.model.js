71d728d61b34681e6981ae51c98c6ba06eea02b7
arturo <ahpareja@gmail.com>
2023-03-11 17:28:58 -0500

    Validaciones modelo mensaje

--- src/dao/models/message.model.js
+++ src/dao/models/message.model.js
@@ -1,7 +1,19 @@
 const mongoose = require('mongoose');
 
+const validateEmail = (email) => {
+	const re = /\S+@\S+\.\S+/;
+	return re.test(email);
+};
+
 const messageSchema = new mongoose.Schema({
-	user: String,
+	user: {
+		type: String,
+		required: 'Email is required',
+		lowercase: true,
+		trim: true,
+		validate: [validateEmail, 'Please fill a valid email address'],
+		match: [/\S+@\S+\.\S+/, 'is invalid']
+	},
 	message: String
 });
 
--- src/routes/chat.router.js
+++ src/routes/chat.router.js
@@ -4,9 +4,13 @@ const MessageManager = require('../dao/chatManager.mongo');
 const chatManager = new MessageManager();
 
 router.post('/', async (req, res) => {
-	const { user, message } = req.body;
-	await chatManager.addMessage(user, message);
-	res.status(200).json({ message: 'Message sent' });
+	try {
+		const { user, message } = req.body;
+		await chatManager.addMessage(user, message);
+		res.status(200).json({ message: 'Message added successfully' });
+	} catch (error) {
+		res.status(400).json({ message: '[!] Email validation error.' })
+	}
 });
 
 router.get('/', async (req, res) => {
